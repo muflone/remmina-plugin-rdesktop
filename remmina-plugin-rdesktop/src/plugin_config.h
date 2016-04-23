071f35bc5049fe504bc51caac0a3b5e82a01c81b
Muflone <webreg@vbsimple.net>
2016-04-24 00:29:34 +0200

    Updated for Remmina API change and Builder 1.2.3.0

--- remmina-plugin-rdesktop/src/plugin_config.h
+++ remmina-plugin-rdesktop/src/plugin_config.h
@@ -24,6 +24,6 @@
 
   #define PLUGIN_NAME        "RDESKTOP"
   #define PLUGIN_DESCRIPTION "RDESKTOP - Open a RDP connection with rdesktop"
-  #define PLUGIN_VERSION     "1.2.2.0"
+  #define PLUGIN_VERSION     "1.2.3.0"
   #define PLUGIN_APPICON     "remmina-rdesktop"
 #endif
--- remmina-plugin-rdesktop/src/remmina_plugin.c
+++ remmina-plugin-rdesktop/src/remmina_plugin.c
@@ -399,7 +399,8 @@ static RemminaProtocolPlugin remmina_plugin =
   remmina_plugin_rdesktop_close_connection,     // Plugin close connection
   NULL,                                         // Query for available features
   NULL,                                         // Call a feature
-  NULL                                          // Send a keystroke
+  NULL,                                         // Send a keystroke
+  NULL                                          // Capture screenshot
 };
 
 G_MODULE_EXPORT gboolean remmina_plugin_entry(RemminaPluginService *service)
